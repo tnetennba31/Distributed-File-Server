d341c967fb62f38a3f6eebc3b5b68128fc5cd66a
Joshua <jb6321@engr.ship.edu>
2020-12-03 14:08:02 -0500

    added possible way to refactor methods (commented out)

--- DistributedFileClient/src/menuStateMachine/ReadAction.java
+++ DistributedFileClient/src/menuStateMachine/ReadAction.java
@@ -1,4 +1,7 @@
 package menuStateMachine;
+import java.io.File;
+
+import java.io.FileNotFoundException;
 
 import java.util.Scanner;
 
@@ -24,5 +27,31 @@ public class ReadAction extends MenuAction
 		String input = keyboard.nextLine();
 		System.out.println(DistributedFileClient.getDistributedFileImpl().csOpenRead(input));
 	}
+	
+//	@Override
+//	void execute()
+//	{
+//		Scanner keyboard = new Scanner(System.in);
+//		System.out.println("Enter the name of the file to open for read:");
+//		String input = keyboard.nextLine();
+////		System.out.println(DistributedFileClient.getDistributedFileImpl().csOpenRead(input));
+//		
+//		try {
+//		String path = DistributedFileClient.getDistributedFileImpl().csOpenRead(input);
+//		Scanner s = new Scanner(new File(path));
+//		StringBuffer contents = new StringBuffer("");
+//		while (s.hasNext()) {
+//			contents.append(s.nextLine() + "\n");
+//		}
+//
+//		s.close();
+//		FileManager.doneReading(path);
+//
+////		return contents.toString();
+//		System.out.println(contents.toString());
+//	} catch (FileNotFoundException e) {
+//	}
+//		
+//	}
 
 }
